69018900ec898bf3a76be7a3995982b70dceb24b
Shahzeb <mdshahzeb@live.in>
2025-05-05 23:57:17 +0530

    feat: add Netlify adapter and update configuration for improved deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,11 +6,14 @@ import sitemapPlugin from '@astrojs/sitemap';
 
 import react from '@astrojs/react';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss(), sitemapPlugin()]
   },
 
-  integrations: [react()]
+  integrations: [react()],
+  adapter: netlify()
 });
